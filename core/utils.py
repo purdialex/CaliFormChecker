0761df37213b77b315ea8c8979a7d2576fca4bd6
purdialex <purdialex67@gmail.com>
2025-06-14 17:56:38 +0300

    Did the draw angle inside the utils.py

--- core/utils.py
+++ core/utils.py
@@ -1,3 +1,4 @@
+import cv2
 import numpy as np
 
 def get_coords(landmarks, landmark_enum):
@@ -41,3 +42,20 @@ def calculate_angle(a, b, c):
     cosine_angle = np.dot(ba, bc) / (np.linalg.norm(ba) * np.linalg.norm(bc))
     angle = np.arccos(np.clip(cosine_angle, -1.0, 1.0))
     return np.degrees(angle)
+
+
+def draw_angle(img, name, pt1, pt2, pt3, color, h, w):
+    """
+    Draws the angle between three points on the image.
+
+    Parameters:
+    - img: The image to draw on
+    - name: Name of the angle (not used now, but helpful for debugging or labeling)
+    - pt1, pt2, pt3: The three keypoints
+    - color: Color of the angle text
+    - h, w: Height and width of the image
+    """
+    angle = calculate_angle(pt1, pt2, pt3)
+    px = int(pt2[0] * w), int(pt2[1] * h)
+    cv2.putText(img, f'{int(angle)}°', (px[0], px[1] - 10),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2, cv2.LINE_AA)
--- modes/streamlit_squat_camera.py
+++ modes/streamlit_squat_camera.py
@@ -2,7 +2,7 @@ import cv2
 import mediapipe as mp
 from streamlit_webrtc import VideoProcessorBase
 from core.squat_checker_complex import SquatChecker
-from core.utils import get_main_body_points, calculate_angle
+from core.utils import get_main_body_points, draw_angle
 
 mp_pose = mp.solutions.pose
 mp_drawing = mp.solutions.drawing_utils
@@ -24,32 +24,33 @@ class SquatCameraProcessor(VideoProcessorBase):
 
         if results.pose_landmarks:
             self.checker.update(results.pose_landmarks, mp_pose)
-            mp_drawing.draw_landmarks(
-                img,
-                results.pose_landmarks,
-                mp_pose.POSE_CONNECTIONS,
-                mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
-                mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
-            )
+            mp_drawing.draw_landmarks(img, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
 
             keypoints = get_main_body_points(results.pose_landmarks, mp_pose)
             h, w, _ = img.shape
 
-            def draw_angle(name, pt1, pt2, pt3, color):
-                angle = calculate_angle(pt1, pt2, pt3)
-                px = int(pt2[0] * w), int(pt2[1] * h)
-                cv2.putText(img, f'{int(angle)}°', (px[0], px[1] - 10),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2, cv2.LINE_AA)
 
-            draw_angle("left_arm", keypoints["left_shoulder"], keypoints["left_elbow"], keypoints["left_wrist"], (255, 0, 0))
-            draw_angle("right_arm", keypoints["right_shoulder"], keypoints["right_elbow"], keypoints["right_wrist"], (255, 0, 0))
-            draw_angle("left_leg", keypoints["left_hip"], keypoints["left_knee"], keypoints["left_ankle"], (0, 0, 255))
-            draw_angle("right_leg", keypoints["right_hip"], keypoints["right_knee"], keypoints["right_ankle"], (0, 0, 255))
-            draw_angle("left_torso", keypoints["left_shoulder"], keypoints["left_hip"], keypoints["left_knee"], (0, 255, 255))
-            draw_angle("right_torso", keypoints["right_shoulder"], keypoints["right_hip"], keypoints["right_knee"], (0, 255, 255))
 
-            cv2.putText(img, f'Good Squats: {self.checker.counter}', (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-            cv2.putText(img, f'Mid Squats: {self.checker.partial_counter}', (10, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-            cv2.putText(img, f'Feedback: {self.checker.feedback}', (10, 120), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
+            draw_angle(img, "left_arm", keypoints["left_shoulder"], keypoints["left_elbow"], keypoints["left_wrist"],
+                       (255, 0, 0), h, w)
+            draw_angle(img, "right_arm", keypoints["right_shoulder"], keypoints["right_elbow"],
+                       keypoints["right_wrist"], (255, 0, 0), h, w)
+            draw_angle(img, "left_leg", keypoints["left_hip"], keypoints["left_knee"], keypoints["left_ankle"],
+                       (0, 0, 255), h, w)
+            draw_angle(img, "right_leg", keypoints["right_hip"], keypoints["right_knee"], keypoints["right_ankle"],
+                       (0, 0, 255), h, w)
+            draw_angle(img, "left_torso", keypoints["left_shoulder"], keypoints["left_hip"], keypoints["left_knee"],
+                       (0, 255, 255), h, w)
+            draw_angle(img, "right_torso", keypoints["right_shoulder"], keypoints["right_hip"], keypoints["right_knee"],
+                       (0, 255, 255), h, w)
+
+            cv2.putText(img, f'Good Squats: {self.checker.counter}', (10, 40),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            cv2.putText(img, f'Feedback: {self.checker.feedback}', (10, 80),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1,
+                        (255, 0, 0) if self.checker.feedback == "GET LOWER!" else (0, 255, 255), 2)
+            cv2.putText(img, f'Mid Squats: {self.checker.partial_counter}', (10, 120),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
         return frame.from_ndarray(img, format="bgr24")
